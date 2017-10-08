500d4d2bfc21a612dd094a9980bbb52c86418c36
pjastr <24464854+pjastr@users.noreply.github.com>
2017-10-08 17:45:31 +0200

    Add files via upload

---
+++ ZRozw1-7/ZRozw1-7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZRozw1_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pierwszaLiczba = 10, drugaLiczba = 20, i=5, j=-3, k=77; //trzeba zainicjować zmienna aby się do nich odwołać
+            Console.WriteLine("pierwszaLiczba: " + pierwszaLiczba);
+            Console.WriteLine("drugaLiczba: " + drugaLiczba);
+            Console.WriteLine("zmienna i: " + i);
+            Console.WriteLine("zmienna j: " + j);
+            Console.WriteLine("zmienna k: " + k);
+            Console.ReadKey();
+        }
+    }
+}
